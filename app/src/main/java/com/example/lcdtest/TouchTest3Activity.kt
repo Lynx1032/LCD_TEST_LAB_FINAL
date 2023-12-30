64f068545b4da451437b89fa42380a87b17f54ce
Lynx1032 <hohoangnghiep.2002@gmail.com>
2023-12-30 21:44:30 +0700

    Final change before release

--- app/src/main/java/com/example/lcdtest/TouchTest3Activity.kt
+++ app/src/main/java/com/example/lcdtest/TouchTest3Activity.kt
@@ -1,3 +1,14 @@
+/*
+Đồ án TH Lập trình di động
+GVHD: ThS Nguyễn Thái Công Nghĩa
+
+Nhóm 2A:
+    - Hồ Hoàng Nghiệp - 20200277
+    - Nguyễn Tiến Trung - 20200382
+
+Ứng dụng Test màn hình điện thoại
+ */
+
 package com.example.lcdtest
 
 import android.content.Context
